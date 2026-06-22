7d94c0596c810f89e22d641edb4355ae358df4d5
anuragsharma2512 <anuragsharma4213@gmail.com>
2026-06-22 23:23:56 +0530

    add language feature to my project

--- src/nodes/blog_node.py
+++ src/nodes/blog_node.py
@@ -1,4 +1,6 @@
 from src.states.blogstate import BlogState
+from langchain_core.messages import SystemMessage, HumanMessage
+from src.states.blogstate import Blog
 
 class BlogNode:
     """
@@ -33,3 +35,41 @@ class BlogNode:
             system_message = system_prompt.format(topic=state["topic"])
             response = self.llm.invoke(system_message)
             return {"blog": {"title": state['blog']['title'], "content": response.content}}
+        
+    def translation(self,state:Blog):
+        """
+        Translate the content to the specified language.
+        """
+
+        translation_prompt="""
+        Translate the following content into {current_language}.
+        - Maintain the original tone, style and formatting.
+        - Adapt cultural references and idioms to be appropriate for {current_language}.
+
+        ORIGINAL CONTENT:
+        {blog_content}
+        
+        """
+
+        blog_content=state["blog"]["content"]
+        message=[
+            HumanMessage(translation_prompt.format(current_langurage=state["current_language"],blog_content=blog_content))
+        ]
+        translation_content = self.llm.with_structured_output(Blog).invoke(message)
+
+    
+    def route(self,state:BlogState):
+        return {"current_language":state['current_language']}
+    
+
+    def route_decision(self,state:BlogState):
+        """
+        Route the content to the respective translation function
+        """
+
+        if state["current_language"]=="hindi":
+            return "hindi"
+        elif state["current_language"]=="french":
+            return "french"
+        else:
+            return state["current_language"]
